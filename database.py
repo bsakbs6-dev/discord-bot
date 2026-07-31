2c75a9b9a9e9175f5d261ec75dd6571dbe627584
bsakbs6-dev <bsakbs6@gmail.com>
2026-07-31 12:23:39 +0300

    Update database.py

--- database.py
+++ database.py
@@ -1,36 +1,62 @@
 import aiosqlite
 
-DATABASE = "moderation.db"
+DATABASE_NAME = "legionmoder.db"
 
 
-async def init_db():
-    async with aiosqlite.connect(DATABASE) as db:
+async def init_database():
+    async with aiosqlite.connect(DATABASE_NAME) as db:
 
         await db.execute("""
         CREATE TABLE IF NOT EXISTS checks(
             user_id INTEGER PRIMARY KEY,
-            moderator_id INTEGER,
-            expires_at INTEGER
+            moderator_id INTEGER NOT NULL,
+            expires_at INTEGER NOT NULL
         )
         """)
 
         await db.execute("""
         CREATE TABLE IF NOT EXISTS punishments(
             id INTEGER PRIMARY KEY AUTOINCREMENT,
-            user_id INTEGER,
-            moderator_id INTEGER,
-            type TEXT,
-            reason TEXT,
-            expires_at INTEGER
+            user_id INTEGER NOT NULL,
+            moderator_id INTEGER NOT NULL,
+            type TEXT NOT NULL,
+            reason TEXT NOT NULL,
+            expires_at INTEGER NOT NULL
         )
         """)
 
         await db.execute("""
-        CREATE TABLE IF NOT EXISTS clan(
+        CREATE TABLE IF NOT EXISTS roster(
             user_id INTEGER PRIMARY KEY,
-            nickname TEXT,
-            privilege TEXT
+            nickname TEXT NOT NULL,
+            privilege TEXT NOT NULL
         )
         """)
 
         await db.commit()
+
+
+async def execute(query: str, parameters=()):
+    async with aiosqlite.connect(DATABASE_NAME) as db:
+        await db.execute(query, parameters)
+        await db.commit()
+
+
+async def fetchone(query: str, parameters=()):
+    async with aiosqlite.connect(DATABASE_NAME) as db:
+
+        cursor = await db.execute(query, parameters)
+
+        row = await cursor.fetchone()
+
+        return row
+
+
+async def fetchall(query: str, parameters=()):
+    async with aiosqlite.connect(DATABASE_NAME) as db:
+
+        cursor = await db.execute(query, parameters)
+
+        rows = await cursor.fetchall()
+
+        return rows
