3f31f64986f82aa51db56e9e3fd861dd1ee9fe58
bsakbs6-dev <bsakbs6@gmail.com>
2026-07-31 12:06:52 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
 import os
 import discord
 from discord.ext import commands
+from discord import app_commands
 from dotenv import load_dotenv
+
+import config
 
 load_dotenv()
 
 TOKEN = os.getenv("TOKEN")
 
 intents = discord.Intents.default()
-intents.guilds = True
 intents.members = True
+intents.guilds = True
 intents.message_content = True
 
 bot = commands.Bot(
@@ -17,14 +20,24 @@ bot = commands.Bot(
     intents=intents
 )
 
+
+def has_any_role(member: discord.Member, roles: list[int]) -> bool:
+    return any(role.id in roles for role in member.roles)
+
+
 @bot.event
 async def on_ready():
-    print(f"✅ Бот запущен как {bot.user}")
+    print("=" * 40)
+    print(f"Бот запущен как {bot.user}")
+    print(f"discord.py {discord.__version__}")
 
     try:
         synced = await bot.tree.sync()
-        print(f"Синхронизировано {len(synced)} команд.")
+        print(f"Slash-команд синхронизировано: {len(synced)}")
     except Exception as e:
         print(e)
+
+    print("=" * 40)
+
 
 bot.run(TOKEN)
